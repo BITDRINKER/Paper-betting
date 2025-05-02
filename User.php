004ff8e661e107f0fcfabc496ebe080114ee9443
BITDRINKER <161921883+BITDRINKER@users.noreply.github.com>
2025-05-02 21:27:37 +0630

    Create User.php

---
+++ User.php
@@ -0,0 +1,8 @@
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable {
+    protected $fillable = ['name', 'email', 'password'];
+    protected $hidden = ['password'];
+}
